c54cb5df48ccd76e5ca38c9dc54cbaa5d328cfe6
guchi-bottom <s1712033@s.do-johodai.ac.jp>
2020-10-01 17:35:15 +0900

    10/01 17:35 進捗保存

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -14,6 +14,9 @@ namespace WindowsFormsApplication1
     public partial class Form1 : Form
     {
         string[] saved_data;
+
+        bool new_contents = true;
+        int contents_id = 0;
         public Form1()
         {
             InitializeComponent();
@@ -47,7 +50,7 @@ namespace WindowsFormsApplication1
                 }
                 file.Close();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
@@ -57,16 +60,53 @@ namespace WindowsFormsApplication1
         {
             //id,name,service,status,date_1,log_1,date_2,log_2,date_3,log_3,date_4,log_4,date_5,log_5,
             //date_6,log_6,date_7,log_7,date_8,log_8,date_9,log_9,date_10,log_10
+
+            //企業名, サービス情報取得
             string name = name_company.Text, service = name_service.Text;
-            if(name != "" && service != "")
+
+            //活動状況取得
+            int i_status = 0;
+            foreach (RadioButton log in group_log.Controls) //エラー
+            /*型 'System.InvalidCastException' のハンドルされていない例外が Graduations.exe で発生しました
+              追加情報:型 'System.Windows.Forms.Button' のオブジェクトを型 'System.Windows.Forms.RadioButton' にキャストできません。*/
             {
-               int id = saved_data.Length + 1;
-               
+                switch (log.Text)
+                {
+                    case "log_doing":
+                        i_status = 1;
+                        break;
+                    case "log_unofficial":
+                        i_status = 2;
+                        break;
+                    case "log_decline":
+                        i_status = 3;
+                        break;
+                }
             }
-            
+
+            //活動履歴確認
+            MessageBox.Show(date_1st.Value.ToString(), "chech");
         }
 
-        private void group_status_Enter(object sender, EventArgs e)
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == 0)
+            {
+                new_contents = true;
+                contents_id = saved_data.Length;
+                data_add.Text = "新規追加";
+                data_del.Text = "クリア";
+            }
+            else
+            {
+                new_contents = false;
+                contents_id = listBox1.SelectedIndex;
+                data_add.Text = "更新";
+                data_del.Text = "削除";
+            }
+        }
+
+        private void data_del_Click(object sender, EventArgs e)
         {
 
         }
